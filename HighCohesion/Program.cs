e965c3793253c4a353c6e558aa5ff56fed8039ca
Max <max_address@yahoo.com>
2018-01-18 12:25:55 +0100

    1e refatoring vers HighCohesion et LowCoulpling

--- HighCohesion/Program.cs
+++ HighCohesion/Program.cs
@@ -6,70 +6,68 @@ namespace HighCohesion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HighCohesion Hello");
+            Console.WriteLine("HighCohesion & LowCoupling Hello");
             Sale sale = new Sale();
             sale.Price = 100;
 
+            CustomerManager cm = new CustomerManager();
+            SaleManager sm = new SaleManager();
             ECommerce ec = new ECommerce();
-            if (ec.makePayment(sale))
+
+            if (cm.accountExist())
             {
-                Console.WriteLine("Paiement OK !");
+                if (sm.addSale(sale))
+                {
+                    if (ec.makePayment(sale))
+                    {
+                        Console.WriteLine("Paiement OK !");
+                    }
+                }
             }
+        }
+    }
+
+    public class CustomerManager
+    {
+        UserService userSrv = new UserService();
+
+        public bool accountExist()
+        {
+            return userSrv.accountExist();
+        }
+
+        public bool createAccount()
+        {
+            return userSrv.createAccount();
+        }
+    }
+
+    public class SaleManager
+    {
+        SalesService salesSrv = new SalesService();
+
+        public bool addSale(Sale currentSale)
+        {
+            return salesSrv.addSale(currentSale);
         }
     }
 
     public class ECommerce
     {
         public bool makePayment(Sale currentSale) {
-            // On vérifie si le compte de l'utilisateur existe et si non, on le crée
-            UserService userSrv = new UserService();
-            SalesService salesSrv = new SalesService();
+            
             PaymentService paySrv = new PaymentService();
-
-            if (userSrv.accountExist())
+           
+            // on fait le paiement
+            if (paySrv.performPayment(currentSale.Price))
             {
-                // on stocke la vente
-                if (salesSrv.addSale(currentSale))
-                {
-                    // on fait le paiement
-                    if (paySrv.performPayment(currentSale.Price))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
             }
             else
             {
-                userSrv.createAccount();
-
-                 // on stocke la vente
-                if (salesSrv.addSale(currentSale))
-                {
-                    // on fait le paiement
-                    if (paySrv.performPayment(currentSale.Price))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-                else
-                {
-                    return false;
-                }
+                return false;
             }
         }
-
     }
 
     internal class PaymentService
@@ -86,9 +84,9 @@ namespace HighCohesion
         
     }
 
-    internal class SalesService
+    public class SalesService
     {
-        internal bool addSale(Sale currentSale)
+        public bool addSale(Sale currentSale)
         {
             // Ajoute la vente
             return true;
@@ -97,13 +95,14 @@ namespace HighCohesion
 
     public class UserService
     {
-        internal bool accountExist()
+        public bool accountExist()
         {
             return true;
         }
 
-        internal void createAccount()
+        public bool createAccount()
         {
+            return true;
             // crée le compte
         }
     }
