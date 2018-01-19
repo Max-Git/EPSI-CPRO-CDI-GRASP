a5f73933568b3ffd8e9f5b02f6a3cb101b57be2b
Max <max_address@yahoo.com>
2018-01-19 14:41:17 +0100

    Pattern Information Expert et Creator OK

--- InformationExpert/Program.cs
+++ InformationExpert/Program.cs
@@ -11,18 +11,23 @@ namespace InformationExpert
             Console.WriteLine("InformationExpert");
 
             Product p1 = new Product(){Id=1, Name="p1", Price=10};
-            Product p2 = new Product(){Id=1, Name="p2", Price=150};
-            Product p3 = new Product(){Id=1, Name="p3", Price=50};
+            Product p2 = new Product(){Id=2, Name="p2", Price=150};
+            Product p3 = new Product(){Id=3, Name="p3", Price=50};
 
-            CommandLine cl1 = new CommandLine(){CLProduct = p1, Quantity = 2};
-            CommandLine cl2 = new CommandLine(){CLProduct = p2, Quantity = 10};
-            CommandLine cl3 = new CommandLine(){CLProduct = p3, Quantity = 1};
+            CommandLine cl1 = new CommandLine();
+            CommandLine cl2 = new CommandLine();
+            CommandLine cl3 = new CommandLine();
+
+            cl1.AddProduct(p1, 10);
+            cl2.AddProduct(p2, 50);
+            cl3.AddProduct(p3, 5);
 
             Command c = new Command();
             c.CommandDate = DateTime.Now;
-            c.CommandLines.Add(cl1);
-            c.CommandLines.Add(cl2);
-            c.CommandLines.Add(cl3);
+
+            c.AddCommandLine(cl1);
+            c.AddCommandLine(cl2);
+            c.AddCommandLine(cl3);
 
             Console.WriteLine("Total Cmd : "+ c.GetTotal());
         }
@@ -37,13 +42,19 @@ namespace InformationExpert
             CommandLines = new List<CommandLine>();
         }
 
+        public void AddCommandLine(CommandLine cl)
+        {
+            // ....
+            this.CommandLines.Add(cl);
+        }
+
         internal int GetTotal()
         {
             int res = 0;
             
             foreach (CommandLine item in CommandLines)
             {
-                res = res + (item.Quantity * item.CLProduct.Price); 
+                res = res + item.GetSubTotal(); 
             }
 
             return res;
@@ -54,6 +65,17 @@ namespace InformationExpert
     {
         public int Quantity { get; set; }
         public Product CLProduct { get; set; }
+
+        public int GetSubTotal()
+        {
+            return this.Quantity * this.CLProduct.GetPrice();
+        }
+
+        public void AddProduct (Product p, int qte)
+        {
+            this.CLProduct = p;
+            this.Quantity = qte;
+        }
     }
 
     public class Product
@@ -61,5 +83,10 @@ namespace InformationExpert
         public int Price { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public int GetPrice ()
+        {
+            return this.Price;
+        }
     }
 }
